e76c245a618d3ad8368ee00e666c1305e8085e60
jaywcjlove <398188662@qq.com>
2022-10-01 11:59:29 +0800

    feat: add remembered theme.

--- scripts/utils/darkMode.mjs
+++ scripts/utils/darkMode.mjs
@@ -2,10 +2,17 @@ import path from 'path';
 import { getSVGNode } from './getSVGNode.mjs';
 
 const scripts = `
+  const LOCAL_NANE = '_dark_mode_theme_'
+  const rememberedValue = localStorage.getItem(LOCAL_NANE);
+  if (rememberedValue && ['light', 'dark'].includes(rememberedValue)) {
+    document.documentElement.setAttribute('data-color-mode', rememberedValue);
+  }
   const button = document.querySelector('#darkMode');
   button.onclick = () => {
     const theme = document.documentElement.dataset.colorMode;
-    document.documentElement.setAttribute('data-color-mode', theme === 'light' ? 'dark' : 'light');
+    const mode = theme === 'light' ? 'dark' : 'light';
+    document.documentElement.setAttribute('data-color-mode', mode);
+    localStorage.setItem(LOCAL_NANE, mode);
   }
 `;
 
