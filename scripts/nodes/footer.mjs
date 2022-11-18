48ccc06c3bd432ff46ba66a47c19deba95c3834f
Mofe Lee <mofe@me.com>
2022-11-18 13:49:56 +0800

    fix: footer timezone bug

--- scripts/nodes/footer.mjs
+++ scripts/nodes/footer.mjs
@@ -3,7 +3,10 @@ import formatter from '@uiw/formatter';
 export function footer(options = {}) {
   let footerText = '© 2022 Kenny Wang.';
   if (options.isHome) {
-    footerText += ` Updated on ${formatter('YYYY/MM/DD HH:mm:ss', new Date())}`;
+    const now = new Date();
+    const utc = now.getTime() + now.getTimezoneOffset() * 60000;
+    const cst = new Date(utc + 3600000 * 8);
+    footerText += ` Updated on ${formatter('YYYY/MM/DD HH:mm:ss', cst)}`;
   }
   return {
     type: 'element',
