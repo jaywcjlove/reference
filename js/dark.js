7e9fc4b991af2412a822c4ae6052f6df0e14c65e
jaywcjlove <jaywcjlove@users.noreply.github.com>
2023-06-06 06:04:26 +0000

    chore: update dependency refs-cli to v1.4.1 4d813c765f4e8364e811c3c18b62486cd1bf198e

--- js/dark.js
+++ js/dark.js
@@ -1,4 +1,4 @@
-const LOCAL_NANE = '_dark_mode_theme_'
+const LOCAL_NANE = '_dark_mode_theme_';
 const rememberedValue = localStorage.getItem(LOCAL_NANE);
 if (rememberedValue && ['light', 'dark'].includes(rememberedValue)) {
   document.documentElement.setAttribute('data-color-mode', rememberedValue);
@@ -9,4 +9,4 @@ button.onclick = () => {
   const mode = theme === 'light' ? 'dark' : 'light';
   document.documentElement.setAttribute('data-color-mode', mode);
   localStorage.setItem(LOCAL_NANE, mode);
-}
+};
